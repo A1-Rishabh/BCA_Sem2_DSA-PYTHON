95b32de49cfb84ab1602f9b434eb67696702b030
A1-Rishabh <rishavbhardwaj2007@gmail.com>
2026-02-02 23:31:07 +0530

    practice

--- Practice/practice.py
+++ Practice/practice.py
@@ -36,12 +36,33 @@
 
 
 #convert decimal to binary
-n=(int(input("Enter a Number: ")))
-binary=0
-x=1
-for i in range(0,32):
-    rem=n%2
-    binary=binary+rem*x
-    x=x*10
-    n=n//2
-print(binary)
+# n=(int(input("Enter a Number: ")))
+# binary=0
+# x=1
+# for i in range(0,32):
+#     rem=n%2
+#     binary=binary+rem*x
+#     x=x*10
+#     n=n//2
+# print(binary)
+
+#move zeros to end of array
+nums=[0,1,0,3,12]
+nonzero=0
+for i in range(0,len(nums)):
+    if nums[i]!=0:
+        nums[i],nums[nonzero]=nums[nonzero],nums[i]
+        nonzero+=1
+print(nums)
+
+# count the number of even and odd elements in an array
+arr=[12,3,4,5,6,7,8,9]
+even_count=0
+odd_count=0
+for i in range(len(arr)):
+    if arr[i]%2==0:
+        even_count+=1 
+    else:
+        odd_count+=1
+print("even count is:",even_count)
+print("odd count is:",odd_count)
