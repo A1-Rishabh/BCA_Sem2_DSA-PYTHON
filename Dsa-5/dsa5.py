64acf2eaf77f8466493106ba6ffd12dcf10cf18f
A1-Rishabh <rishavbhardwaj2007@gmail.com>
2026-02-13 14:11:02 +0530

    sum of primary diagonal and secondary diagonal

--- Dsa-5/dsa5.py
+++ Dsa-5/dsa5.py
@@ -12,18 +12,18 @@ for i in range(rows):
 
 print("Matrix")
 
-# traversal of matrix 
+# # traversal of matrix 
 for i in range(rows):
     for j in range(columns):
         print(matrix[i][j],end=" ")
     print()
 
-# visiting primary diagonal
-for i in range(rows):
-    print(matrix[i][i],end=" ")
-# visiting secondary diagonal
-for i in range(rows):
-    print(matrix[i][columns-1-i],end=" ")
+# # visiting primary diagonal
+# for i in range(rows):
+#     print(matrix[i][i],end=" ")
+# # visiting secondary diagonal
+# for i in range(rows):
+#     print(matrix[i][columns-1-i],end=" ")
 
 # # visiting all elements in row wise manner
 # for i in range(rows):
@@ -33,3 +33,17 @@ for i in range(rows):
 # for j in range(columns):
 #     for i in range(rows):
 #         print(matrix[i][j],end=" ")
+# # calculate the sum of rows  and columns
+# for i in range(rows):
+#     sum=0
+#     sumcol=0
+#     for j in range(columns):
+#         sum=sum+matrix[i][j]
+#         sumcol=sumcol+matrix[j][i]
+#     print("sum of rows: ",sum)
+#     print("sum of columns: ",sumcol)
+#sum of primary diagonal
+sum=0
+for i in range(rows):
+    sum=sum+matrix[i][i]
+print("sum of primary diagonal: ",sum)
